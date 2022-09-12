0caf2262ad3ad10c966876790791a59fab3453af
escaping-samsara <jpusterhofer@gmail.com>
2022-09-12 22:30:25 +0200

    first push before wordcloud inegration

---
+++ kaiserfranzjosef.py
@@ -0,0 +1,25 @@
+#import worldpan
+import random
+import time
+
+
+
+def printQuestion(frage, antworten):
+    print("MEINE FRAGE LAUTET: \n {}\nThe Answer IS: {} ".format(frage, antworten) )
+
+def chooseName(antworten):
+    index = random.randint(0, 3)
+    name = antworten[index]
+    return name
+
+
+
+frage = "Wie heisst du denn mein sohn?"
+antworten = ["Kaiser Franz Josef", "FUCK", "Berndt", "PUTIN"]
+
+for i in range(0,10):
+    time.sleep(3)
+    printQuestion(frage, chooseName(antworten))
+
+
+
